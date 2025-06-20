828c73c607cb1e5754e67cac4d642e58c15cc68e
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2025-06-20 15:03:43 +0100

    Change copy and show page navigation buttons within the viewport of the form builder entry point UI

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export default function FormBuilder() {
   return (
-    <div className="h-[100dvh] flex items-center justify-center">
+    <div className="h-full flex items-center justify-center">
       <Button asChild>
-        <Link href="/builder/info">Show form pages</Link>
+        <Link href="/builder/info">Start Editing</Link>
       </Button>
     </div>
   );
